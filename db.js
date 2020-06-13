5027fdb0baecdc28c7b9020ea34aac71e94b9918
Gabriel <gabo6252@gmail.com>
2020-06-13 16:09:37 -0400

    Cambios en server.js

--- db.js
+++ db.js
@@ -22,3 +22,10 @@ module.exports = pool;
 // DB_PORT=3306
 // DB_NAME=taller3
 // JWT_SECRET=randomsecretvalue
+
+// DB_HOST=bovdhxjhq6l1k5hh1h4e-mysql.services.clever-cloud.com
+// DB_USER=ulatok3gsmv7xxwm
+// DB_PASS=LfD1aNGGLptw5p68fnrO
+// DB_PORT=3306
+// DB_NAME=bovdhxjhq6l1k5hh1h4e
+// JWT_SECRET=randomsecretvaluepassenger
